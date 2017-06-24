a9ffa19b91cb641733e7286dc2e119db3b9b0888
Marvin Calero <MJCRequeno@gmail.com>
2017-06-24 14:47:19 -0600

    agregado indentacion y y modificado boton para moviles

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,7 @@
              var $target = $(this.hash);
              $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
              if ($target.length) {
-                 var targetOffset = $target.offset().top;
+                 var targetOffset = $target.offset().top-115;
                  $('html,body').animate({scrollTop: targetOffset}, 1000);
                  return false;
             }
