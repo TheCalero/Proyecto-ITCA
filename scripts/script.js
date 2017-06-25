8e1b0f546fcd2a32432d6f203221e071e07a3649
Marvin Calero <MJCRequeno@gmail.com>
2017-06-25 12:54:39 -0600

    agregar archivos 2

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@
     $('.parallax').parallax();
     $('.tooltipped').tooltip({delay: 50});
     $('.slider').slider({interval: 3600, transition:600});
+    $('.collapsible').collapsible();
     $('a[href*=#]').click(function() {
      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
          && location.hostname == this.hostname) {
